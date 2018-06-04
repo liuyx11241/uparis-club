631760fa437b33dd71f60ded30afce8c054b6023
Yanxi LIU <liuyx11241@users.noreply.github.com>
2018-06-04 13:20:07 +0200

    Init Commit

---
+++ uparis-core/src/main/java/com/uparisclub/UparisCoreApplication.java
@@ -0,0 +1,12 @@
+package com.uparisclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UparisCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UparisCoreApplication.class, args);
+	}
+}
