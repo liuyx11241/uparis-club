2cf4318bdd8a83675ee3cda1161ed311f891215c
Yanxi LIU <liuyx11241@users.noreply.github.com>
2018-07-18 21:12:47 +0200

    Sticky footer

--- uparis-mdb/src/app/eshop/eshop.component.ts
+++ uparis-mdb/src/app/eshop/eshop.component.ts
@@ -3,6 +3,17 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'eshop',
     templateUrl: './eshop.component.html',
+    styles: [`
+        :host {
+            display: flex;
+            flex-flow: column;
+            height: 100vh;
+        }
+
+        #main {
+            flex: 1;
+        }
+    `]
 })
 export class EshopComponent {
     constructor() {
